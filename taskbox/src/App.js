a2f2bcdedbd02e546912a5cb734e875e13c2b213
danylolviv <61681583+danylolviv@users.noreply.github.com>
2020-06-05 21:14:56 +0300

    taskbox/

---
+++ taskbox/src/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import { Provider } from 'react-redux';
+import store from './lib/redux';
+import InboxScreen from './components/InboxScreen';
+
+import TaskList from './components/TaskList';
+import './index.css';
+
+class App extends Component {
+  render() {
+    return (
+      <Provider store={store}>
+        <InboxScreen />
+      </Provider>
+    );
+  }
+}
+
+export default App;
